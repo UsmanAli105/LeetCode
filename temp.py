83f62ea75940e97c8987bfd873e219470f0833fe
Syed Muhammad Usman Ali Bukhari <73774067+UsmanAli105@users.noreply.github.com>
2023-08-15 16:11:24 +0500

    Doing new task

---
+++ temp.py
@@ -0,0 +1,32 @@
+d = {
+    'A': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    'B': [1, 2, 3, 4, 5,],
+    'C': [1, 2, 3]
+}
+
+
+for i,j in d.items():
+    print(i, j)
+
+from sklearn.preprocessing import OneHotEncoder
+
+# Apply one-hot encoder to each column with categorical data
+OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(X_train[object_cols]))
+OH_cols_valid = pd.DataFrame(OH_encoder.transform(X_valid[object_cols]))
+
+# One-hot encoding removed index; put it back
+OH_cols_train.index = X_train.index
+OH_cols_valid.index = X_valid.index
+
+# Remove categorical columns (will replace with one-hot encoding)
+num_X_train = X_train.drop(object_cols, axis=1)
+num_X_valid = X_valid.drop(object_cols, axis=1)
+
+# Add one-hot encoded columns to numerical features
+OH_X_train = pd.concat([num_X_train, OH_cols_train], axis=1)
+OH_X_valid = pd.concat([num_X_valid, OH_cols_valid], axis=1)
+
+# Ensure all columns have string type
+OH_X_train.columns = OH_X_train.columns.astype(str)
+OH_X_valid.columns = OH_X_valid.columns.astype(str)
